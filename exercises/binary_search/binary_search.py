98817db7ceb8103273676f46f27193537b0bfc16
Leonardo Augusto <sleonardoaugusto@gmail.com>
2021-07-06 22:33:49 -0300

    Prime numbers binary search

--- exercises/binary_search/binary_search.py
+++ exercises/binary_search/binary_search.py
@@ -9,3 +9,48 @@ def search(n):
             _min = guess
         else:
             return guess
+
+
+def is_prime(n):
+    _min_index = 0
+    _max_index = len(primes) - 1
+    while True:
+        half = (_min_index + _max_index) // 2
+        guess = primes[half]
+        if n == guess:
+            return True
+        elif half == 0:
+            return False
+        elif n < guess:
+            _max_index = half - 1
+        elif n > guess:
+            _min_index = half + 1
+
+
+primes = [
+    2,
+    3,
+    5,
+    7,
+    11,
+    13,
+    17,
+    19,
+    23,
+    29,
+    31,
+    37,
+    41,
+    43,
+    47,
+    53,
+    59,
+    61,
+    67,
+    71,
+    73,
+    79,
+    83,
+    89,
+    97,
+]
--- exercises/binary_search/test_binary_search.py
+++ exercises/binary_search/test_binary_search.py
@@ -1,4 +1,4 @@
-from exercises.binary_search.binary_search import search
+from exercises.binary_search.binary_search import search, is_prime
 
 
 def test_should_return_2_500_000_000_000():
@@ -15,3 +15,27 @@ def test_should_return_7_500_000_000_000():
 
 def test_should_return_10_000_000_000_000():
     assert search(n=10_000_000_000_000) == 10_000_000_000_000
+
+
+def test_2_should_be_prime():
+    assert is_prime(n=2)
+
+
+def test_3_should_be_prime():
+    assert is_prime(n=2)
+
+
+def test_5_should_be_prime():
+    assert is_prime(n=2)
+
+
+def test_4_should_not_be_prime():
+    assert not is_prime(n=4)
+
+
+def test_6_should_not_be_prime():
+    assert not is_prime(n=4)
+
+
+def test_8_should_not_be_prime():
+    assert not is_prime(n=4)
